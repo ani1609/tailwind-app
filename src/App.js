8d5d4052f5d762c69f473b76845a26cb3c0c7d36
Ankit Kr Chowdhury <89239354+ani1609@users.noreply.github.com>
2023-11-05 17:44:06 +0530

    basic tailwind environment setup

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App()
     return (
         <div className="App">
             <div className="bg-blue-200 p-4">
-                <h1 className="text-2xl font-bold text-blue-700">Hello, Tailwind CSS!</h1>
+                <h1 className="text-2xl font-bold text-blue-700 text-center">Hello, Tailwind CSS!</h1>
             </div>
         </div>
     );
